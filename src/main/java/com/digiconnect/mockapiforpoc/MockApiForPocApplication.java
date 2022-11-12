f16a8763a6217fb5bd15ace49a2cf194bdc2bde1
oprabin <ooprabin@gmail.com>
2022-11-12 15:40:53 +0545

    Initial Setup.

---
+++ src/main/java/com/digiconnect/mockapiforpoc/MockApiForPocApplication.java
@@ -0,0 +1,13 @@
+package com.digiconnect.mockapiforpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockApiForPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockApiForPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digiconnect/mockapiforpoc/MockApiForPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digiconnect.mockapiforpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockApiForPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
